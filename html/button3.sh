76f57ae8fef9c036eae750942cb4dc39deab5e95
root <root@localhost.localdomain>
2021-04-25 16:29:04 +0300

    final_commit

---
+++ html/button3.sh
@@ -0,0 +1,1 @@
+echo "you clicked button3"
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum install -y epel-release && \
+yum install nginx php-fpm && \
+\
+mkdir -p /var/www && \
+cp -r $HOME/sysbuttons/html /var/www && \
+chown apache.apache /var/www -R && \
+chmod 755 /var/www -R && \
+\
+rm -rfv /etc/nginx/nginx.conf && rm -rfv /etc/nginx/*default && /etc/nginx/default.d && /etc/nginx/conf.d && \
+cp -r $HOME/sysbuttons/nginx/nginx.conf /etc/nginx.conf && \
+\
+sed -i s/"expose_php = Off"/"expose_php = On"/g /etc/php.ini && \
+\
+systemctl enable nginx && systemctl restart nginx && \
+systemctl enable php-fpm && systemctl restart nginx
